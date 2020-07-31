149484fc8875288804a7887596c9cb5ee335b9d5
Tony Rosler <tony.rosler246@gmail.com>
2020-08-01 00:58:55 +0200

    From 4 curl requests down to 2.

--- oneliner.sh
+++ oneliner.sh
@@ -1,2 +1,3 @@
-{ curl -s "ifconfig.me" >> /dev/null 2>&1 \
- && printf "%s - %s, %s.\n" "$(curl -s ipinfo.io/ip)" "$(curl -s ipinfo.io/city)" "$(curl -s ipinfo.io/country)"; } || echo "lost - somewhere off the shoulder of Orion";
+{ curl -s "ifconfig.me" >> /dev/null 2>&1 && RESP=$(curl -s "ipinfo.io") && \
+ printf "%s - %s, %s.\n" "$(grep -Eo '"ip":.*?[^\\],' <<< "$RESP" | sed 's/^.*: "//;s/",//')" "$(grep -Eo '"city":.*?[^\\],' <<< "$RESP" | sed 's/^.*: "//;s/",//')" "$(grep -Eo '"country":.*?[^\\],' <<< "$RESP" | sed 's/^.*: "//;s/",//')"; } || \
+  echo "lost - somewhere off the shoulder of Orion";
