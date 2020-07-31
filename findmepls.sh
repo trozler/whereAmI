9b344ca0ac13a7653c3104c665a3834d984f4bf3
patrick-doland <39927882+patrick-doland@users.noreply.github.com>
2020-07-31 16:22:49 -0400

    Copy edit to correct typo.

--- findmepls.sh
+++ findmepls.sh
@@ -6,4 +6,4 @@ set -eE
   RESP=$(curl -s "ifconfig.me")
   LOC=$(curl -s "https://tools.keycdn.com/geo.json?host={$RESP}")
   printf "%s - %s, %s.\n" "$RESP" "$(grep -Eo '"region_name":.*?[^\\],' <<< "$LOC" | sed 's/^.*:"//;s/",//')" "$(grep -Eo '"country_name":.*?[^\\],' <<< "$LOC" | sed 's/^.*:"//;s/",//')"
-} || echo "lost - somewhere of the shoulder of Orion"
+} || echo "lost - somewhere off the shoulder of Orion"
