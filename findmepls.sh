8fe25d8410f360da99effa09e285afaa9e9c6d7a
Tony Rosler <tony.rosler246@gmail.com>
2020-07-31 20:37:00 +0200

    spelling mistake corrected.

--- findmepls.sh
+++ findmepls.sh
@@ -3,4 +3,4 @@
 { RESP=$(curl -s "ifconfig.me") \
  && LOC=$(curl -s "https://tools.keycdn.com/geo.json?host={$RESP}") \
   && printf "%s - %s, %s.\n" "$RESP" "$(grep -Eo '"region_name":.*?[^\\],' <<< "$LOC" | sed 's/^.*:"//;s/",//')" "$(grep -Eo '"country_name":.*?[^\\],' <<< "$LOC" | sed 's/^.*:"//;s/",//')"; } \
-  || echo "lost - somehwere of the shoulder of Orion";
+  || echo "lost - somewhere off the shoulder of Orion";
