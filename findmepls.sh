f44d2b934359628f50182d2766e1a05c4959204a
Tony Rosler <tony.rosler246@gmail.com>
2020-08-01 01:15:24 +0200

    Small spelling edit.

--- findmepls.sh
+++ findmepls.sh
@@ -2,4 +2,4 @@
 
 { curl -s "ifconfig.me" >> /dev/null 2>&1 && RESP=$(curl -s "ipinfo.io") && \
  printf "%s - %s, %s.\n" "$(grep -Eo '"ip":.*?[^\\],' <<< "$RESP" | sed 's/^.*: "//;s/",//')" "$(grep -Eo '"city":.*?[^\\],' <<< "$RESP" | sed 's/^.*: "//;s/",//')" "$(grep -Eo '"country":.*?[^\\],' <<< "$RESP" | sed 's/^.*: "//;s/",//')"; } || \
-  echo "lost - somewhere off the shoulder of Orion";
+  echo "lost - somewhere off the shoulder of Orion.";
