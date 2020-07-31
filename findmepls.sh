b8a206f979f0fd0101e7a9e769cdbcb428300d2d
Tony Rosler <tony.rosler246@gmail.com>
2020-07-31 20:19:58 +0200

    no longer one line. my b

--- findmepls.sh
+++ findmepls.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-{ RESP=$(curl -s "ifconfig.me") && LOC=$(curl -s "https://tools.keycdn.com/geo.json?host={$RESP}") && printf "%s - %s, %s.\n" "$RESP" "$(grep -Eo '"region_name":.*?[^\\],' <<< "$LOC" | sed 's/^.*:"//;s/",//')" "$(grep -Eo '"country_name":.*?[^\\],' <<< "$LOC" | sed 's/^.*:"//;s/",//')"; } || echo "lost - somehwere of the shoulder of Orion";
+{ RESP=$(curl -s "ifconfig.me") \
+ && LOC=$(curl -s "https://tools.keycdn.com/geo.json?host={$RESP}") \
+  && printf "%s - %s, %s.\n" "$RESP" "$(grep -Eo '"region_name":.*?[^\\],' <<< "$LOC" | sed 's/^.*:"//;s/",//')" "$(grep -Eo '"country_name":.*?[^\\],' <<< "$LOC" | sed 's/^.*:"//;s/",//')"; } \
+  || echo "lost - somehwere of the shoulder of Orion";
