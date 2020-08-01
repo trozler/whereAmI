2ba27ff9ca27aac96e09e19febfa67c2bac02139
Tony Rosler <tony.rosler246@gmail.com>
2020-08-01 13:40:47 +0200

    Reformatted some stuff.

--- findmepls.sh
+++ findmepls.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 { curl -s "ifconfig.me" >> /dev/null 2>&1 && RESP=$(curl -s "ipinfo.io") && \
- printf "%s - %s, %s.\n" "$(grep -Eo '"ip":.*?[^\\],' <<< "$RESP" | sed 's/^.*: "//;s/",//')" "$(grep -Eo '"region":.*?[^\\],' <<< "$RESP" | sed 's/^.*: "//;s/",//')" "$(grep -Eo '"country":.*?[^\\],' <<< "$RESP" | sed 's/^.*: "//;s/",//')"; } || \
+ printf "%s - %s, %s.\n" "$(grep -Eo '"ip":.*?[^\\],' <<< "$RESP" | sed 's/^.*: "//;s/",//')" \
+  "$(grep -Eo '"region":.*?[^\\],' <<< "$RESP" | sed 's/^.*: "//;s/",//')" \
+   "$(grep -Eo '"country":.*?[^\\],' <<< "$RESP" | sed 's/^.*: "//;s/",//')"; } || \
   echo "lost - somewhere off the shoulder of Orion.";
