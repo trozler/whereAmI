d3e6ab0392b7f9f01cd1575fc2fe002ef7df9d23
Tony Rosler <tony.rosler246@gmail.com>
2020-08-01 14:33:08 +0200

    Reformatted and made more faster.

--- findmepls.sh
+++ findmepls.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-{ curl -s "ifconfig.me" >> /dev/null 2>&1 && RESP=$(curl -s "ipinfo.io") && \
- printf "%s - %s, %s.\n" "$(grep -Eo '"ip":.*?[^\\],' <<< "$RESP" | sed 's/^.*: "//;s/",//')" \
+{ 
+    RESP=$(curl -s "ipinfo.io") && \
+ printf "%s - %s, %s.\n" \
+  "$(grep -Eo '"ip":.*?[^\\],' <<< "$RESP" | sed 's/^.*: "//;s/",//')" \
   "$(grep -Eo '"region":.*?[^\\],' <<< "$RESP" | sed 's/^.*: "//;s/",//')" \
-   "$(grep -Eo '"country":.*?[^\\],' <<< "$RESP" | sed 's/^.*: "//;s/",//')"; } || \
-  echo "lost - somewhere off the shoulder of Orion.";
+   "$(grep -Eo '"country":.*?[^\\],' <<< "$RESP" | sed 's/^.*: "//;s/",//')"; 
+   } || echo "lost - somewhere off the shoulder of Orion.";
